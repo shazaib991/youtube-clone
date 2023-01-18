b0396df61da7a5c6c3f05603886a4b0f195ddeee
shazaib991 <shazaib424647@gmail.com>
2023-01-18 21:04:42 +0500

    updated header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,54 +1,85 @@
 import Image from "next/image";
 import MicIcon from "@mui/icons-material/Mic";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useState } from "react";
 
 export const Header = () => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = (e) => {
+    setSearchText(e.currentTarget.value);
+  };
+
   return (
     <div className="w-full h-[56px] flex  items-center justify-between px-[18px]">
-      <div className="flex items-center">
-        <div className="w-[38px] h-[38px] flex justify-center items-center rounded-[50%] hover:bg-black/10 cursor-pointer">
-          <div className="w-[18px] h-[13px] flex flex-col justify-between">
-            <div className="w-full h-[1px] bg-black"></div>
-            <div className="w-full h-[1px] bg-black"></div>
-            <div className="w-full h-[1px] bg-black"></div>
+      <div className="flex">
+        <div className="flex items-center">
+          <div className="w-[38px] h-[38px] flex justify-center items-center rounded-[50%] hover:bg-black/10 cursor-pointer">
+            <div className="w-[18px] h-[13px] flex flex-col justify-between">
+              <div className="w-full h-[1px] bg-black"></div>
+              <div className="w-full h-[1px] bg-black"></div>
+              <div className="w-full h-[1px] bg-black"></div>
+            </div>
+          </div>
+          <div className="flex items-center ml-[17px]">
+            <Image
+              src={"/assets/yt_logo_rgb_light.png"}
+              width={90}
+              height={20}
+              alt="youtube logo"
+            />
           </div>
         </div>
-        <div className="flex items-center ml-[17px]">
-          <Image
-            src={"/assets/yt_logo_rgb_light.png"}
-            width={90}
-            height={20}
-            alt="youtube logo"
-          />
-        </div>
-      </div>
-      <div className="flex items-center ml-[18px]">
-        <div>
-          <input
-            type="text"
-            name="youtube-search"
-            id="youtube-search"
-            placeholder="Search"
-            style={{
-              boxShadow: "inset 0px 3px 6px -6px #888",
-            }}
-            className="w-[540px] h-[40px] border border-[#d6d6d6] rounded-tl-[40px] rounded-bl-[40px] outline-none pl-[17px] focus:border-[blue]"
-          />
-        </div>
-        <div className="w-[65px] h-[40px] flex items-center bg-[#f8f8f8] border border-[#d6d6d6] border-l-0 rounded-tr-[40px] rounded-br-[40px] cursor-pointer hover:bg-[#e8e8e8]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="currentColor"
-            className="bi bi-search ml-[20px]"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-          </svg>
-        </div>
-        <div className="w-[40px] h-[40px] flex justify-center items-center bg-[#707070]/5 rounded-[50%] hover:bg-black/10 ml-[8px] cursor-pointer">
-          <MicIcon sx={{ fontSize: "22px" }} />
+        <div className="flex items-center ml-[168px]">
+          <div className="w-[563px] h-[40px] flex items-center justify-end relative">
+            <input
+              type="text"
+              name="youtube-search"
+              id="youtube-search"
+              placeholder="Search"
+              onChange={handleSearch}
+              value={searchText}
+              className="w-[95%] h-full border border-[#d6d6d6] rounded-tl-[40px] rounded-bl-[40px] outline-none pl-[17px] focus:border-[blue] focus:w-full peer focus:pl-[45px] shadow-[inset_0_1px_1px_rgba(50,50,50,0.1)] focus:shadow-[inset_0_1px_2px_rgba(50,50,50,0.4)]"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              fill="currentColor"
+              className="bi bi-search ml-[20px] absolute left-0 hidden peer-focus:block"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="29"
+              height="29"
+              fill="currentColor"
+              class={`bi bi-x absolute cursor-pointer ${
+                searchText.length > 0 ? "bloack" : "hidden"
+              }`}
+              onClick={() => setSearchText("")}
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+          </div>
+          <div className="w-[65px] h-[40px] flex items-center bg-[#f8f8f8] border border-[#d6d6d6] border-l-0 rounded-tr-[40px] rounded-br-[40px] cursor-pointer hover:bg-[#e8e8e8]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="currentColor"
+              className="bi bi-search ml-[20px]"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </div>
+          <div className="w-[40px] h-[40px] flex justify-center items-center bg-[#707070]/5 rounded-[50%] hover:bg-black/10 ml-[8px] cursor-pointer">
+            <MicIcon sx={{ fontSize: "22px" }} />
+          </div>
         </div>
       </div>
       <div className="flex items-center">
