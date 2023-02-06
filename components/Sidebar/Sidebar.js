289080c606df8190729344e7a3c0196b87e2ccfd
shazaib991 <shazaib424647@gmail.com>
2023-02-06 16:28:55 +0500

    sidebar updated

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -2,11 +2,33 @@ import VideoLibraryIcon from "@mui/icons-material/VideoLibrary";
 import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import WatchLaterIcon from "@mui/icons-material/WatchLater";
+import WhatshotOutlinedIcon from "@mui/icons-material/WhatshotOutlined";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import MusicNoteOutlinedIcon from "@mui/icons-material/MusicNoteOutlined";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import VideogameAssetOutlinedIcon from "@mui/icons-material/VideogameAssetOutlined";
+import FeedIcon from "@mui/icons-material/Feed";
+import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
+import EmojiEventsOutlinedIcon from "@mui/icons-material/EmojiEventsOutlined";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import SettingsIcon from "@mui/icons-material/Settings";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import FlagIcon from "@mui/icons-material/Flag";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import HelpIcon from "@mui/icons-material/Help";
+import FeedbackOutlinedIcon from "@mui/icons-material/FeedbackOutlined";
+import FeedbackIcon from "@mui/icons-material/Feedback";
 
 export const Sidebar = () => {
+  const date = new Date();
+
   return (
     <div className="w-[240px] h-full fixed overflow-y-auto invisible hover:visible">
-      <div className="visible pt-[14px]">
+      <div className="h-[1115px] visible pt-[14px]">
         <a
           className="flex items-center bg-[#f2f2f2] hover:bg-[#e6e6e6] mx-[11px] py-[9px] rounded-[11px]"
           href="/"
@@ -16,7 +38,7 @@ export const Sidebar = () => {
             width="16"
             height="16"
             fill="currentColor"
-            className="bi bi-house hidden ml-[18px]"
+            className="bi bi-house hidden ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z" />
@@ -26,13 +48,13 @@ export const Sidebar = () => {
             width="20"
             height="20"
             fill="currentColor"
-            className="bi bi-house-fill ml-[18px]"
+            className="bi bi-house-fill ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5Z" />
             <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6Z" />
           </svg>
-          <p className="text-[14px] ml-[25px] font-medium">Home</p>
+          <p className="text-[14px] ml-[26px] font-medium">Home</p>
         </a>
         <a
           className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
@@ -43,7 +65,7 @@ export const Sidebar = () => {
             width="20"
             height="20"
             fill="currentColor"
-            class="bi bi-play ml-[18px]"
+            className="bi bi-play ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" />
@@ -53,12 +75,12 @@ export const Sidebar = () => {
             width="20"
             height="20"
             fill="currentColor"
-            class="bi bi-play-fill hidden ml-[18px]"
+            className="bi bi-play-fill hidden ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
           </svg>
-          <p className="text-[14px] ml-[25px]">Shorts</p>
+          <p className="text-[14px] ml-[26px]">Shorts</p>
         </a>
         <a
           className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
@@ -69,7 +91,7 @@ export const Sidebar = () => {
             width="20"
             height="20"
             fill="currentColor"
-            class="bi bi-collection-play ml-[18px]"
+            className="bi bi-collection-play ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="M2 3a.5.5 0 0 0 .5.5h11a.5.5 0 0 0 0-1h-11A.5.5 0 0 0 2 3zm2-2a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 0-1h-7A.5.5 0 0 0 4 1zm2.765 5.576A.5.5 0 0 0 6 7v5a.5.5 0 0 0 .765.424l4-2.5a.5.5 0 0 0 0-.848l-4-2.5z" />
@@ -80,35 +102,35 @@ export const Sidebar = () => {
             width="20"
             height="20"
             fill="currentColor"
-            class="bi bi-play-fill hidden ml-[18px]"
+            className="bi bi-play-fill hidden ml-[16px]"
             viewBox="0 0 16 16"
           >
             <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
           </svg>
-          <p className="text-[14px] ml-[25px]">Subscriptions</p>
+          <p className="text-[14px] ml-[26px]">Subscriptions</p>
         </a>
         <hr className="mt-[14px] mb-[12px]" />
         <a
           className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
           href="/"
         >
-          <VideoLibraryOutlinedIcon className="ml-[18px] text-[20px]" />
-          <VideoLibraryIcon className="hidden ml-[18px] text-[20px]" />
-          <p className="text-[14px] ml-[25px]">Library</p>
+          <VideoLibraryOutlinedIcon className="ml-[16px] text-[20px]" />
+          <VideoLibraryIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Library</p>
         </a>
         <a
           className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
           href="/"
         >
-          <AccessTimeIcon className="ml-[18px] text-[20px]" />
-          <VideoLibraryIcon className="hidden ml-[18px] text-[20px]" />
-          <p className="text-[14px] ml-[25px]">History</p>
+          <AccessTimeIcon className="ml-[16px] text-[20px]" />
+          <WatchLaterIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">History</p>
         </a>
         <hr className="mt-[14px] mb-[14px]" />
-        <p className="ml-[36px] text-[14px] mb-[12px]">
+        <p className="ml-[34px] text-[14px] mb-[12px]">
           Sign in to like videos, comment and subscribe.
         </p>
-        <div className="h-[35px] w-[103px] flex items-center border ml-[36px] pl-[10px] pr-[15px] rounded-[40px] cursor-pointer">
+        <div className="h-[35px] w-[103px] flex items-center border ml-[34px] pl-[10px] pr-[15px] rounded-[40px] cursor-pointer">
           <div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -127,9 +149,171 @@ export const Sidebar = () => {
           </div>
           <p className="text-[15px] font-[500] text-[blue]">Sign in</p>
         </div>
-        <hr className="mt-[16px] mb-[14px]" />
+        <hr className="mt-[16px] mb-[16px]" />
+        <p className="ml-[25px] mb-[5px]">Explore</p>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <WhatshotOutlinedIcon className="ml-[16px] text-[20px]" />
+          <WhatshotIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Trending</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <MusicNoteOutlinedIcon className="ml-[16px] text-[20px]" />
+          <MusicNoteIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Music</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <VideogameAssetOutlinedIcon className="ml-[16px] text-[20px]" />
+          <VideogameAssetIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Gaming</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <FeedOutlinedIcon className="ml-[16px] text-[20px]" />
+          <FeedIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">News</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <EmojiEventsOutlinedIcon className="ml-[16px] text-[20px]" />
+          <EmojiEventsIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Sports</p>
+        </a>
+        <hr className="mt-[14px] mb-[13px]" />
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <AddCircleOutlineIcon className="ml-[16px] text-[20px]" />
+          <AddCircleIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Browse channels</p>
+        </a>
+        <hr className="mt-[14px] mb-[13px]" />
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <SettingsOutlinedIcon className="ml-[16px] text-[20px]" />
+          <SettingsIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Settings</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <FlagOutlinedIcon className="ml-[16px] text-[20px]" />
+          <FlagIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Report history</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <HelpOutlineOutlinedIcon className="ml-[16px] text-[20px]" />
+          <HelpIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Help</p>
+        </a>
+        <a
+          className="flex items-center hover:bg-[#f2f2f2] mx-[11px] py-[9px] rounded-[11px]"
+          href="/"
+        >
+          <FeedbackOutlinedIcon className="ml-[16px] text-[20px]" />
+          <FeedbackIcon className="hidden ml-[16px] text-[20px]" />
+          <p className="text-[14px] ml-[26px]">Send feedback</p>
+        </a>
+        <hr className="mt-[14px] mb-[13px]" />
+        <div className="mx-[26px] leading-[18px]">
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Press
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Copyright
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            Contact us
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Creators
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            Advertise
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Developers
+          </a>
+        </div>
+        <div className="ml-[26px] mt-[8px] leading-[18px]">
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            Terms
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Privacy
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] ml-[8px] inline-block font-medium"
+          >
+            Policy & safety
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            How YouTube works
+          </a>
+          <a
+            href="#"
+            className="text-[13px] text-[#606060] inline-block font-medium"
+          >
+            Test new Features
+          </a>
+          <p className="text-[12px] text-[#93929E] mt-[15px]">
+            &copy; {date.getFullYear()}
+          </p>
+        </div>
       </div>
-      <div className="h-[800px]"></div>
     </div>
   );
 };
